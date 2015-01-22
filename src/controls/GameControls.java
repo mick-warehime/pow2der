2a6b1b95287679d778e8d8e22ea95a4fe473322a
dvirk <dvir.kafri@gmail.com>
2015-01-21 19:13:38 -0500

    Modified slick2d's input.java class to check if a given controller is a Wiimote. If so, then the joystick move tolerance is lowered.

--- src/controls/GameControls.java
+++ src/controls/GameControls.java
@@ -20,7 +20,7 @@ import commands.MoveCommand;
 public class GameControls {
 
 	private int[] mousePos = new int[2];
-	private InputProvider keyboardInputProvider;
+	private InputProvider controlsInputProvider;
 	
 	public GameControls(GameContainer gc){
 		initializeKeyBindings(gc);
@@ -37,21 +37,21 @@ public class GameControls {
 
 	public void addPlayerListener(KeyboardInputListener listener) {
 		// TODO Auto-generated method stub
-		keyboardInputProvider.addListener(listener);
+		controlsInputProvider.addListener(listener);
 	}
 	
 	private void initializeKeyBindings(GameContainer gc){
 		Controller[] controllers=ControllerEnvironment.getDefaultEnvironment().getControllers();
 		
 		for (Controller c : controllers){			
-			if(c.getName().equals("Wiimote (18-2a-7b-44-b0-bc)")){
+			if(c.getName().contains("Wiimote")){
 				joystick jstick = new joystick(c.getType());
 				System.out.println(jstick.getX_LeftJoystick_Value());
 			}
 		}
 		
 		//This translates keyboard/mouse inputs into commands, for the appropriate listeners
-		keyboardInputProvider = new InputProvider(gc.getInput());
+		controlsInputProvider = new InputProvider(gc.getInput());
 		//The listener is linked to the provider		
 
 		//Define action commands for provider
@@ -65,20 +65,20 @@ public class GameControls {
 		
 
 		//Bind commands to keyboard keys
-		keyboardInputProvider.bindCommand(new KeyControl(Input.KEY_A), moveLeft);
-		keyboardInputProvider.bindCommand(new KeyControl(Input.KEY_D), moveRight);
-		keyboardInputProvider.bindCommand(new KeyControl(Input.KEY_W), moveUp);
-		keyboardInputProvider.bindCommand(new KeyControl(Input.KEY_S), moveDown);
-		keyboardInputProvider.bindCommand(new KeyControl(Input.KEY_F), interact);
+		controlsInputProvider.bindCommand(new KeyControl(Input.KEY_A), moveLeft);
+		controlsInputProvider.bindCommand(new KeyControl(Input.KEY_D), moveRight);
+		controlsInputProvider.bindCommand(new KeyControl(Input.KEY_W), moveUp);
+		controlsInputProvider.bindCommand(new KeyControl(Input.KEY_S), moveDown);
+		controlsInputProvider.bindCommand(new KeyControl(Input.KEY_F), interact);
 
 		
 		//Bind commands to controller keys
-		keyboardInputProvider.bindCommand(new ControllerDirectionControl(0, ControllerDirectionControl.LEFT), moveLeft);
-		keyboardInputProvider.bindCommand(new ControllerDirectionControl(0, ControllerDirectionControl.RIGHT), moveRight);
-		keyboardInputProvider.bindCommand(new ControllerDirectionControl(0, ControllerDirectionControl.UP), moveUp);
-		keyboardInputProvider.bindCommand(new ControllerDirectionControl(0, ControllerDirectionControl.DOWN), moveDown);
+		controlsInputProvider.bindCommand(new ControllerDirectionControl(0, ControllerDirectionControl.LEFT), moveLeft);
+		controlsInputProvider.bindCommand(new ControllerDirectionControl(0, ControllerDirectionControl.RIGHT), moveRight);
+		controlsInputProvider.bindCommand(new ControllerDirectionControl(0, ControllerDirectionControl.UP), moveUp);
+		controlsInputProvider.bindCommand(new ControllerDirectionControl(0, ControllerDirectionControl.DOWN), moveDown);
 //		keyboardInputProvider.bindCommand(new ControllerButtonControl(0,1),);
-		keyboardInputProvider.bindCommand(new ControllerButtonControl(0,2), interact);
+		controlsInputProvider.bindCommand(new ControllerButtonControl(0,2), interact);
 		//The left bumper is 5, the right is 6
 	}
 }
