04690f6a34c2f78ca8d7f2ce2518fb851c50172b
dvirk <dvir.kafri@gmail.com>
2015-02-26 12:15:02 -0500

    Further worked on pasring item info

--- src/items/Item.java
+++ src/items/Item.java
@@ -21,10 +21,10 @@ import actors.Status;
 
 public class Item extends BasicObject implements Interactive{
 
-	
-	
-	
-	
+
+
+
+
 	private ItemProperties properties = new ItemProperties();
 	private ItemLocation location = new ItemLocation(this);
 	private static float SPRITE_DRAW_SCALE = 0.6f;
@@ -34,51 +34,51 @@ public class Item extends BasicObject implements Interactive{
 	public Item(Map<String, String> itmInfo, Image image, int xPos, int yPos) throws SlickException{		
 
 		super(image,xPos,yPos);
-		
+
 		this.definePropertiesFromMap(itmInfo);
-		
+
 		this.graphics.setSpriteMargin(SPRITE_MARGIN);
-		
+
 		//Shrink shape because it's based on image size
 		shrinkShapes();
-		
-			
-		
+
+
+
 		this.canCollide = false;
-		
-		
+
+
 	}
 
 	private void shrinkShapes(){
-		
+
 		((Rectangle)this.shape).grow(-RECT_SHRINK_MARGIN, -RECT_SHRINK_MARGIN);
-		
+
 	}
-	
+
 	public boolean isOnGround(){
 		return location.onGround;
 	}
-	
+
 
 	@Override
 	public void render(Graphics g, int renderX, int renderY){
-		
+
 		if(location.onGround){
 			graphics.render(g, renderX , renderY, SPRITE_DRAW_SCALE);
 		}
 	}
-	
-	
 
-	
 
-	
+
+
+
+
 	public ItemProperties getProperties(){
 		return properties;
 	}
-	
 
-	
+
+
 
 	@Override
 	public void interact(int interactionType, Status status) {
@@ -86,103 +86,111 @@ public class Item extends BasicObject implements Interactive{
 			return;
 		}
 		assert location.onGround : "Error! Item receiving an interact command when it's not on the ground!";
-		
+
 		Inventory inventory = status.getInventory();
 		Shape actorShape = status.getRect();
 		location.applyPickup(inventory, actorShape);
-		
+
 		this.location.onGround = false;
-		
+
 
 	}
-	
+
 	public void drop(){
 		location.applyDrop();
 	}
 
-	
+
 	public void setCurrentLevelData(CurrentLevelData currentLevelData) {
 		location.currentLevelData = currentLevelData;
-		
+
 	}
-	
+
 	/* Handles item's location */
-	
+
 	public class ItemLocation{
-		
-		
-		
+
+
+
 		private Item owningItem;
 		private boolean onGround = true;
 		private Inventory storingInventory;
 		private CurrentLevelData currentLevelData;
 		private Shape ownerShape;
-		
+
 		public ItemLocation(Item owner){
 			this.owningItem = owner;
 		}
-		
-		
-		
+
+
+
 		public void applyDrop(){
 			this.onGround = true;
-			
+
 			float x = ownerShape.getX();
 			float y= ownerShape.getY();
 			ownerShape = null; // Prevents memory leak
-			
+
 			shape.setLocation(x, y);
 			currentLevelData.getCurrentLevel().addObject(owningItem);
 			storingInventory.removeItem(owningItem);
 			storingInventory = null; //Prevents memory leak
 		}
-		
+
 		public void applyPickup(Inventory inventory, Shape actorShape){
 			this.storingInventory = inventory;
 			this.ownerShape = actorShape;
 			storingInventory.addItem(owningItem);
-			
-			
+
+
 		}
-		
-		
-		
+
+
+
 	}
-	
-	
+
+
 	protected void definePropertiesFromMap(Map<String, String> itmInfo){
-		
-		
-		
+
+
+
 		String typeString = itmInfo.get("type");
+
 
 		properties.parseTypeString(typeString);
 
-		System.out.println(this + "has type" + properties.type + ", string was" + typeString);
-		
+
+
 		if (itmInfo.containsKey("weight")){
 			properties.weight = Float.parseFloat(itmInfo.get("weight")); 
 		}else{
 			properties.weight = 0f;
 		}
-		
+
 		String equipLoc = itmInfo.get("equip");
 		
+		properties.parseEquipLocationString(equipLoc);
 		
-		
-		
-		
-		
-		
-		
+		System.out.println("item: " + this + "type: " + properties.type + "location: " + properties.equipLocation);
+
+
+
+
+
+
+
+
+
 	}
-	
+
 	/*
 	 * Permanent properties of an item
 	 * 
 	 */
 	class ItemProperties{
-		
+
+
+		private static final int EQUIP_LOCATION_UNDEFINED = -1;
 		public static final int EQUIP_LOCATION_MAINHAND = 0;
 		public static final int EQUIP_LOCATION_NECK = 1;
 		public static final int EQUIP_LOCATION_CHEST = 2;
@@ -193,7 +201,7 @@ public class Item extends BasicObject implements Interactive{
 		public static final int EQUIP_LOCATION_BACK = 7;
 		public static final int EQUIP_LOCATION_FINGER = 8;
 		public static final int EQUIP_LOCATION_UNEQUIPPABLE = 9;
-		
+
 
 		public static final int TYPE_UNDEFINED = -1;
 		public static final int TYPE_AMULET = 0;
@@ -212,80 +220,119 @@ public class Item extends BasicObject implements Interactive{
 		public static final int TYPE_WAND = 13;
 		public static final int TYPE_STAFF = 14;
 		public static final int TYPE_BOTTLE = 15;
-		
-		
+
+
 		public int equipLocation;
 		public int type;
 		public float weight;
 		public boolean stackable;
-		
+
 		public ItemProperties(){
-			
+
 		}
 
 		public void parseTypeString(String typeString) {
-			
+
 			switch (typeString){
 			case ("amulet"):
 				type = TYPE_AMULET;
-				break;
+			break;
 			case ("book"):
 				type = this.TYPE_BOOK;
-				break;
+			break;
 			case ("boots"):
 				type = this.TYPE_BOOTS;
-				break;
+			break;
 			case ("bottle"):
 				type = this.TYPE_BOTTLE;
-				break;
+			break;
 			case ("chestArmor"):
 				type = this.TYPE_CHESTARMOR;
-				break;
+			break;
 			case ("cloak"):
 				type = this.TYPE_CLOAK;
-				break;
+			break;
 			case ("gloves"):
 				type = this.TYPE_GLOVES;
-				break;
+			break;
 			case ("helm"):
 				type = this.TYPE_HELM;
-				break;
+			break;
 			case ("potion"):
 				type = this.TYPE_POTION;
-				break;
+			break;
 			case ("ring"):
 				type = this.TYPE_RING;
-				break;
+			break;
 			case ("scroll"):
 				type = this.TYPE_SCROLL;
-				break;
+			break;
 			case ("shield"):
 				type = this.TYPE_SHIELD;
-				break;
+			break;
 			case ("staff"):
 				type = this.TYPE_STAFF;
-				break;
+			break;
 			case ("wand"):
 				type = this.TYPE_WAND;
-				break;
+			break;
 			case ("weapon"):
 				type = this.TYPE_WEAPON;
-				break;
+			break;
 			case ("rangedWeapon"):
 				type = this.TYPE_WEAPONRANGED;
-				break;
+			break;
 			default: 
 				type = this.TYPE_UNDEFINED;
 				break;
-			
 			}
-			
+
 		}
-		
-		
-		
+
+		public void parseEquipLocationString(String location) {
+
+			switch (location){
+			case ("neck"):
+				equipLocation = this.EQUIP_LOCATION_NECK;
+			break;
+			case ("chest"):
+				equipLocation = this.EQUIP_LOCATION_CHEST;
+			break;
+			case ("offHand"):
+				equipLocation = this.EQUIP_LOCATION_OFFHAND;
+			break;
+			case ("head"):
+				equipLocation = this.EQUIP_LOCATION_HEAD;
+			break;
+			case ("hands"):
+				equipLocation = this.EQUIP_LOCATION_HANDS;
+			break;
+			case ("back"):
+				equipLocation = this.EQUIP_LOCATION_BACK;
+			break;
+			case ("feet"):
+				equipLocation = this.EQUIP_LOCATION_FEET;
+			break;
+			case ("finger"):
+				equipLocation = this.EQUIP_LOCATION_FINGER;
+			break;
+			case ("mainHand"):
+				equipLocation = this.EQUIP_LOCATION_MAINHAND;
+			break;
+			default: 
+				equipLocation = this.EQUIP_LOCATION_UNDEFINED;
+				break;
+
+			}
+
+
+
+		}
+
+
+
 	}
 
-	
+
 
 }
