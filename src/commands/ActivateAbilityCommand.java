c9d07ae59d90093cccfe06190a40448864120ae8
dvirk <dvir.kafri@gmail.com>
2015-03-09 12:32:40 -0400

    Removed unnecessary imports

--- src/commands/ActivateAbilityCommand.java
+++ src/commands/ActivateAbilityCommand.java
@@ -3,10 +3,8 @@ package commands;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.command.BasicCommand;
 
-import abilities.AbilityObject;
 import actionEngines.ActionEngine;
 import actionEngines.ActorActionEngine;
-import actors.Effect;
 
 
 public class ActivateAbilityCommand extends BasicCommand implements GenericCommand{
