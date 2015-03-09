12cc7d17d78ed22557d7d7a9b3ddd122b7238ac2
dvirk <dvir.kafri@gmail.com>
2015-03-09 19:15:08 -0400

    Made a winded effect that prevents movement. Casting effect no longer affects movement.

--- src/actors/Effect.java
+++ src/actors/Effect.java
@@ -7,6 +7,7 @@ public class Effect implements Removeable {
 	public static final int EFFECT_X_COLLISION = 0;
 	public static final int EFFECT_Y_COLLISION = 1;
 	public static final int EFFECT_COLLIDED_WITH_PLAYER = 2;
+	public static final int EFFECT_WINDED = 3;
 	public static final int EFFECT_INTERACTING = 5;
 	public static final int EFFECT_CASTING_ABILITY = 6;
 	public static final int EFFECT_RUNNING = 7;
@@ -18,8 +19,9 @@ public class Effect implements Removeable {
 	public static final int[] EFFECTS_PREVENTING_ACTION = new int[]
 		{EFFECT_INTERACTING,EFFECT_CASTING_ABILITY};
 	
+	
 	public static final int[] EFFECTS_PREVENTING_MOVEMENT = new int[]
-			{EFFECT_CASTING_ABILITY};
+			{EFFECT_WINDED};
 	
 	public static final int[] EFFECTS_AMBULATING = new int[]
 			{EFFECT_WALKING,EFFECT_RUNNING};
